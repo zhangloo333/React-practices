cdfbc9f41e180ce8cc3e326de1073996a3603e15
Lei Zhang(APA) <leizhang@linkedin.com>
2022-01-05 14:10:53 -0800

    change to use reducer to change the state

--- react-state-management-api/src/index.js
+++ react-state-management-api/src/index.js
@@ -10,46 +10,64 @@ import endpoint from "./endpoint";
 
 import "./styles.scss";
 
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const fetchReducer = (state, action) => {
+  console.log("state", state, "action", action);
+  if (action.type === "LOADING") {
+    return {
+      response: null,
+      loading: true,
+      error: null,
+    };
+  }
+  if (action.type === "RESPONSE_COMPLETE") {
+    return {
+      response: action.payload.response,
+      loading: false,
+      error: null,
+    };
+  }
+
+  if (action.type === "ERROR") {
+    return {
+      response: null,
+      loading: false,
+      error: action.payload.error,
+    };
+  }
+  return state;
+};
+
 const useFetch = (url) => {
-  const [response, setResponse] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = React.useReducer(fetchReducer, initialState);
 
   React.useEffect(() => {
-    setLoading(true);
-    setResponse([]);
-    setError(null);
+    dispatch({ type: "LOADING" });
 
     const fetchUrl = async () => {
       try {
         const response = await fetch(url);
         const data = await response.json(); // must add await otherwise, it won't show up data
-        setResponse(data);
+        dispatch({ type: "RESPONSE_COMPLETE", payload: { response: data } });
       } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+        dispatch({ type: "ERROR", payload: { error } });
       }
     };
 
     fetchUrl();
-
-    // fetch(url)
-    //   .then((response) => response.json())
-    //   .then((response) => {
-    //     setResponse(response);
-    //     setLoading(false);
-    //   })
-    //   .catch((error) => {
-    //     setLoading(false);
-    //     setError(error);
-    //   });
   }, []);
-  return [response, loading, error];
+  console.log("state", state);
+  return [state.response, state.loading, state.error];
 };
 
 const Application = () => {
   const [response, loading, error] = useFetch(endpoint + "/characters");
+  console.log("response", response);
   const characters = (response && response.characters) || [];
 
   return (
