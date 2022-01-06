6b9b7382cd6b9b3a4f1ab13daf6fe49db443c2af
Lei Zhang(APA) <leizhang@linkedin.com>
2022-01-06 11:53:33 -0800

    if you only want to update the one item and all new item, React.memo() and callBack() need to use at the same time.

--- state-management-reduces/src/NewGrudge.js
+++ state-management-reduces/src/NewGrudge.js
@@ -1,36 +1,6 @@
 import React, { useState } from "react";
 
-// const NewGrudge = React.memo(({ onSubmit }) => {
-//   const [person, setPerson] = useState("");
-//   const [reason, setReason] = useState("");
-
-//   const handleChange = (event) => {
-//     event.preventDefault();
-//     onSubmit({ person, reason });
-//   };
-
-//   return (
-//     <form className="NewGrudge" onSubmit={handleChange}>
-//       <input
-//         className="NewGrudge-input"
-//         placeholder="Person"
-//         type="text"
-//         value={person}
-//         onChange={(event) => setPerson(event.target.value)}
-//       />
-//       <input
-//         className="NewGrudge-input"
-//         placeholder="Reason"
-//         type="text"
-//         value={reason}
-//         onChange={(event) => setReason(event.target.value)}
-//       />
-//       <input className="NewGrudge-submit button" type="submit" />
-//     </form>
-//   );
-// });
-
-const NewGrudge = ({ onSubmit }) => {
+const NewGrudge = React.memo(({ onSubmit }) => {
   const [person, setPerson] = useState("");
   const [reason, setReason] = useState("");
 
@@ -58,6 +28,36 @@ const NewGrudge = ({ onSubmit }) => {
       <input className="NewGrudge-submit button" type="submit" />
     </form>
   );
-};
+});
+
+// const NewGrudge = ({ onSubmit }) => {
+//   const [person, setPerson] = useState("");
+//   const [reason, setReason] = useState("");
+
+//   const handleChange = (event) => {
+//     event.preventDefault();
+//     onSubmit({ person, reason });
+//   };
+
+//   return (
+//     <form className="NewGrudge" onSubmit={handleChange}>
+//       <input
+//         className="NewGrudge-input"
+//         placeholder="Person"
+//         type="text"
+//         value={person}
+//         onChange={(event) => setPerson(event.target.value)}
+//       />
+//       <input
+//         className="NewGrudge-input"
+//         placeholder="Reason"
+//         type="text"
+//         value={reason}
+//         onChange={(event) => setReason(event.target.value)}
+//       />
+//       <input className="NewGrudge-submit button" type="submit" />
+//     </form>
+//   );
+// };
 
 export default NewGrudge;
