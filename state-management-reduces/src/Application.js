4b64e5e104f660da0c34266d3bb1a5cbab764557
Lei Zhang(APA) <leizhang@linkedin.com>
2022-01-06 11:22:56 -0800

    refactory to reducer

--- state-management-reduces/src/Application.js
+++ state-management-reduces/src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
 import id from "uuid/v4";
 
@@ -7,22 +7,58 @@ import NewGrudge from "./NewGrudge";
 
 import initialState from "./initialState";
 
-const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+const GRUDGE_ADD = "GRUDGE_ADD";
+const GRUDGE_FORGIVE = "GRUDGE_FORGIVE";
 
-  const addGrudge = (grudge) => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
+const reducer = (state, action) => {
+  if (action.type === GRUDGE_ADD) {
+    return [action.payload, ...state];
+  }
+  if (action.type === GRUDGE_FORGIVE) {
+    return state.map((grudge) => {
+      if (grudge.id !== action.payload.id) return grudge;
+      return { ...grudge, forgiven: !grudge.forgiven };
+    });
+  }
+  return state;
+};
+
+const Application = () => {
+  // const [grudges, setGrudges] = useState(initialState);
+  const [grudges, dispatch] = useReducer(reducer, initialState);
+
+  const addGrudge = ({ person, reason }) => {
+    dispatch({
+      type: GRUDGE_ADD,
+      payload: {
+        person,
+        reason,
+        forgiven: false,
+        id: id(),
+      },
+    });
   };
 
+  // const addGrudge = (grudge) => {
+  //   grudge.id = id();
+  //   grudge.forgiven = false;
+  //   setGrudges([grudge, ...grudges]);
+  // };
+
   const toggleForgiveness = (id) => {
-    setGrudges(
-      grudges.map((grudge) => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
+    dispatch({
+      type: GRUDGE_FORGIVE,
+      payload: {
+        id,
+      },
+    });
+
+    // setGrudges(
+    //   grudges.map((grudge) => {
+    //     if (grudge.id !== id) return grudge;
+    //     return { ...grudge, forgiven: !grudge.forgiven };
+    //   })
+    // );
   };
 
   return (
